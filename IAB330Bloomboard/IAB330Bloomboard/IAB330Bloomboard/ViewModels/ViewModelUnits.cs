4a7159388c6c5d47448ba0db0e53a7bb16130fe6
Fletcher Smart <fletcher208@hotmail.com>
2020-10-07 16:53:36 +1000

    Added viewModelUnits

---
+++ IAB330Bloomboard/IAB330Bloomboard/IAB330Bloomboard/ViewModels/ViewModelUnits.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace IAB330Bloomboard.ViewModels
+{
+    class ViewModelUnits : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
