11ede0a08761da137685f817854911974cc5f587
Lachlan Pierpoint <lachlan.pierpoint@connect.qut.edu.au>
2020-10-14 15:40:54 +1000

    Created grades page

---
+++ IAB330Bloomboard/IAB330Bloomboard/IAB330Bloomboard/ViewModels/ViewModelGrades.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace IAB330Bloomboard.ViewModels
+{
+    class ViewModelMainPage : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public Command NavigateUnits { get; }
+        public Command NavigateAnnouncements { get; }
+
+
+        public ViewModelMainPage()
+        {
+            NavigateUnits = new Command(async () =>
+            {
+                await Application.Current.MainPage.Navigation.PushAsync(new Views.Page1());
+            });
+
+            NavigateAnnouncements = new Command(async () =>
+            {
+                await Application.Current.MainPage.Navigation.PushAsync(new Views.AnnouncementPage());
+            });
+
+        }
+
+    }
+}
