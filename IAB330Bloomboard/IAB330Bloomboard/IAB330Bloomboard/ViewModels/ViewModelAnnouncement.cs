f7022fe3d12efc9c87ad85a7696c4ad2b090d05d
jehiah <jaz@DESKTOP-D9A1C86>
2020-10-14 15:00:22 +1000

    Announcemments navigation

--- IAB330Bloomboard/IAB330Bloomboard/IAB330Bloomboard/ViewModels/ViewModelAnnouncement.cs
+++ IAB330Bloomboard/IAB330Bloomboard/IAB330Bloomboard/ViewModels/ViewModelAnnouncement.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using IAB330Bloomboard.Views;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace IAB330Bloomboard.ViewModels
 {
     class ViewModelAnnouncement : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+        public Command NavigateUnits { get; }
+
+
+        public ViewModelAnnouncement()
+        {
+            Navigate = new Command( async () =>
+            {
+              await  Application.Current.MainPage.Navigation.PushAsync(new Views.Page1());
+            });
+        }
     }
 }
