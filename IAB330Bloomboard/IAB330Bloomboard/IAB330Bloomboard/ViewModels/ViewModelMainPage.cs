7e24114fa93db2541310a586ae4101a66ce0d5b5
Aden <Aden@DESKTOP-DVEJPU9>
2020-10-24 22:05:29 +1000

    Transistioned Mainpage Text to use MVVM-Models

--- IAB330Bloomboard/IAB330Bloomboard/IAB330Bloomboard/ViewModels/ViewModelMainPage.cs
+++ IAB330Bloomboard/IAB330Bloomboard/IAB330Bloomboard/ViewModels/ViewModelMainPage.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using IAB330Bloomboard.Models;
+using IAB330Bloomboard.Views;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
@@ -27,6 +29,64 @@ namespace IAB330Bloomboard.ViewModels
             });
 
         }
+        TextInfo userName = new TextInfo(" ", DateTime.Now, "Welcome Sri");
 
+
+        public string userNameText
+        {
+            get => userName.Text;
+
+        }
+
+        public string userNameTitle
+        {
+            get => userName.Title;
+
+        }
+
+        TextInfo unitTitle = new TextInfo("IAB330", DateTime.Now, "");
+
+
+        public string unitTitleText
+        {
+            get => unitTitle.Text;
+
+        }
+
+        public string unitTitleTitle
+        {
+            get => unitTitle.Title;
+
+        }
+
+        TextInfo unitInfo = new TextInfo("Welcome to Semester 2, 2020 and the IAB330 - Mobile Application Development unit!", DateTime.Now, "Mobile Application Development");
+
+
+        public string unitInfoText
+        {
+            get => unitInfo.Text;
+
+        }
+
+        public string unitInfoTitle
+        {
+            get => unitInfo.Title;
+
+        }
+
+        TextInfo messageInfo = new TextInfo("IAB330 (workshop) is commencing in two hours at GP VIRTOLT10 from 3pm-5pm", DateTime.Now, "Upcoming class");
+
+
+        public string messageInfoText
+        {
+            get => messageInfo.Text;
+
+        }
+
+        public string messageInfoTitle
+        {
+            get => messageInfo.Title;
+
+        }
     }
 }
